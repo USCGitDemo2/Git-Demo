9ecc1f0a119ce9bef7e4c2736b843f27a10365b6
GitDemo2 <usc.gitdemo2@>
2016-02-02 19:41:12 -0800

    I finished the thing!

--- main.cpp
+++ main.cpp
@@ -2,6 +2,12 @@
 
 using namespace std;
 
+void necessaryfeature(){
+	//asdjfl;asdjflas
+	cout << "I am doing well" << endl;
+	cout << "omg I did it" << endl;
+}
+
 int main(){
 	cout << "Hello world" << endl;
 	cout << "New feature" << endl;
